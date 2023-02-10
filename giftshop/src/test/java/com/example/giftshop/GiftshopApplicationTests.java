c17e56fef74d6ea133944ab31cf17904f1ef73c3
Dominic Fraise <dominic.fraise@transferwise.com>
2023-02-10 09:04:19 +0000

    Add basic setup for spring giftshop app

---
+++ giftshop/src/test/java/com/example/giftshop/GiftshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.giftshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
