24977d00b4edbec41654c097e4678ef4ebbd095f
jasonkhan-tw <jason.khan@transferwise.com>
2023-08-25 16:01:40 +0100

    fix merge conflict

--- giftshop/src/main/java/com/example/giftshop/ProductController.java
+++ giftshop/src/main/java/com/example/giftshop/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @RequestMapping("/v1")
